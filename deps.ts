4e24943ee91d815d2ff4785191a2e1d6827a4480
skanehira <skanehira@users.noreply.github.com>
2022-10-18 00:29:07 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/deno-clippy/deno-clippy/deps.ts
    [1/7] Looking for releases: https://deno.land/std@0.159.0/streams/conversion.ts
    [1/7] Attempting update: https://deno.land/std@0.159.0/streams/conversion.ts -> 0.160.0
    [1/7] Update successful: https://deno.land/std@0.159.0/streams/conversion.ts -> 0.160.0
    [2/7] Looking for releases: https://deno.land/std@0.159.0/encoding/base64.ts
    [2/7] Attempting update: https://deno.land/std@0.159.0/encoding/base64.ts -> 0.160.0
    [2/7] Update successful: https://deno.land/std@0.159.0/encoding/base64.ts -> 0.160.0
    [3/7] Looking for releases: https://deno.land/std@0.159.0/io/mod.ts
    [3/7] Attempting update: https://deno.land/std@0.159.0/io/mod.ts -> 0.160.0
    [3/7] Update successful: https://deno.land/std@0.159.0/io/mod.ts -> 0.160.0
    [4/7] Looking for releases: https://deno.land/std@0.159.0/testing/asserts.ts
    [4/7] Attempting update: https://deno.land/std@0.159.0/testing/asserts.ts -> 0.160.0
    [4/7] Update successful: https://deno.land/std@0.159.0/testing/asserts.ts -> 0.160.0
    [5/7] Looking for releases: https://deno.land/std@0.159.0/path/mod.ts
    [5/7] Attempting update: https://deno.land/std@0.159.0/path/mod.ts -> 0.160.0
    [5/7] Update successful: https://deno.land/std@0.159.0/path/mod.ts -> 0.160.0
    [6/7] Looking for releases: https://deno.land/std@0.159.0/streams/mod.ts
    [6/7] Attempting update: https://deno.land/std@0.159.0/streams/mod.ts -> 0.160.0
    [6/7] Update successful: https://deno.land/std@0.159.0/streams/mod.ts -> 0.160.0
    [7/7] Looking for releases: https://deno.land/std@0.159.0/io/buffer.ts
    [7/7] Attempting update: https://deno.land/std@0.159.0/io/buffer.ts -> 0.160.0
    [7/7] Update successful: https://deno.land/std@0.159.0/io/buffer.ts -> 0.160.0
    
    Successfully updated:
    https://deno.land/std@0.159.0/streams/conversion.ts 0.159.0 -> 0.160.0
    https://deno.land/std@0.159.0/encoding/base64.ts 0.159.0 -> 0.160.0
    https://deno.land/std@0.159.0/io/mod.ts 0.159.0 -> 0.160.0
    https://deno.land/std@0.159.0/testing/asserts.ts 0.159.0 -> 0.160.0
    https://deno.land/std@0.159.0/path/mod.ts 0.159.0 -> 0.160.0
    https://deno.land/std@0.159.0/streams/mod.ts 0.159.0 -> 0.160.0
    https://deno.land/std@0.159.0/io/buffer.ts 0.159.0 -> 0.160.0

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export * from "https://deno.land/std@0.159.0/streams/conversion.ts";
-export * as base64 from "https://deno.land/std@0.159.0/encoding/base64.ts";
-export * as io from "https://deno.land/std@0.159.0/io/mod.ts";
-export * from "https://deno.land/std@0.159.0/testing/asserts.ts";
-export * as path from "https://deno.land/std@0.159.0/path/mod.ts";
-export * as streams from "https://deno.land/std@0.159.0/streams/mod.ts";
-export * from "https://deno.land/std@0.159.0/io/buffer.ts";
+export * from "https://deno.land/std@0.160.0/streams/conversion.ts";
+export * as base64 from "https://deno.land/std@0.160.0/encoding/base64.ts";
+export * as io from "https://deno.land/std@0.160.0/io/mod.ts";
+export * from "https://deno.land/std@0.160.0/testing/asserts.ts";
+export * as path from "https://deno.land/std@0.160.0/path/mod.ts";
+export * as streams from "https://deno.land/std@0.160.0/streams/mod.ts";
+export * from "https://deno.land/std@0.160.0/io/buffer.ts";
