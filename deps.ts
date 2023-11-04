19c719c86d5944cbf7791aa02bc75f036206ca13
Alisue <lambdalisue@gmail.com>
2023-11-04 17:59:04 +0900

    :package: Update Deno dependencies

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export * as base64 from "https://deno.land/std@0.204.0/encoding/base64.ts";
-export * as io from "https://deno.land/std@0.204.0/io/mod.ts";
-export * from "https://deno.land/std@0.204.0/testing/asserts.ts";
-export * as path from "https://deno.land/std@0.204.0/path/mod.ts";
-export { copy, readAll } from "https://deno.land/std@0.204.0/streams/mod.ts";
-export * from "https://deno.land/std@0.204.0/io/buffer.ts";
+export * as base64 from "https://deno.land/std@0.205.0/encoding/base64.ts";
+export * as io from "https://deno.land/std@0.205.0/io/mod.ts";
+export * from "https://deno.land/std@0.205.0/testing/asserts.ts";
+export * as path from "https://deno.land/std@0.205.0/path/mod.ts";
+export { copy, readAll } from "https://deno.land/std@0.205.0/streams/mod.ts";
+export * from "https://deno.land/std@0.205.0/io/buffer.ts";
