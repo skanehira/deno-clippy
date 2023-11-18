3c75818c9dd451830b997aa8d497af0186f8450f
skanehira <skanehira@users.noreply.github.com>
2023-11-18 00:17:25 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/deno-clippy/deno-clippy/deps.ts
    [1/3] Looking for releases: https://deno.land/std@0.205.0/encoding/base64.ts
    [1/3] Attempting update: https://deno.land/std@0.205.0/encoding/base64.ts -> 0.207.0
    [1/3] Update successful: https://deno.land/std@0.205.0/encoding/base64.ts -> 0.207.0
    [2/3] Looking for releases: https://deno.land/std@0.205.0/path/mod.ts
    [2/3] Attempting update: https://deno.land/std@0.205.0/path/mod.ts -> 0.207.0
    [2/3] Update successful: https://deno.land/std@0.205.0/path/mod.ts -> 0.207.0
    [3/3] Looking for releases: https://deno.land/std@0.205.0/streams/mod.ts
    [3/3] Attempting update: https://deno.land/std@0.205.0/streams/mod.ts -> 0.207.0
    [3/3] Update successful: https://deno.land/std@0.205.0/streams/mod.ts -> 0.207.0
    
    Successfully updated:
    https://deno.land/std@0.205.0/encoding/base64.ts 0.205.0 -> 0.207.0
    https://deno.land/std@0.205.0/path/mod.ts 0.205.0 -> 0.207.0
    https://deno.land/std@0.205.0/streams/mod.ts 0.205.0 -> 0.207.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export * as base64 from "https://deno.land/std@0.205.0/encoding/base64.ts";
-export * as path from "https://deno.land/std@0.205.0/path/mod.ts";
-export { toText } from "https://deno.land/std@0.205.0/streams/mod.ts";
+export * as base64 from "https://deno.land/std@0.207.0/encoding/base64.ts";
+export * as path from "https://deno.land/std@0.207.0/path/mod.ts";
+export { toText } from "https://deno.land/std@0.207.0/streams/mod.ts";
